addfe97b420ca2fbf7908c5ea1e214334a9d9ff9
Spich <spichakovda@gmail.com>
2023-04-11 05:36:47 +0300

    TableView added

---
+++ FootballChants/ViewControllers/ChantsViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ChantsViewController.swift
+//  FootballChants
+//
+//  Created by Дмитрий Спичаков on 11.04.2023.
+//
+
+import UIKit
+
+class ChantsViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    private lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false // If we build view programmatically
+        tv.backgroundColor = .clear
+        tv.rowHeight = UITableView.automaticDimension
+        tv.estimatedRowHeight = 44
+        tv.separatorStyle = .none
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tv
+    }()
+
+    // MARK: - Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        setup() // Setup tableView (subView)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+private extension ChantsViewController {
+    
+    func setup() {
+        
+        tableView.dataSource = self
+        
+        self.view.addSubview(tableView) // Add subview to View
+        
+        // Anchor view to a points in view
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ChantsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        switch indexPath.row {
+        case 0:
+            cell.backgroundColor = .systemTeal
+        case 1:
+            cell.backgroundColor = .systemGray
+        case 2:
+            cell.backgroundColor = .systemPink
+        default:
+            break
+        }
+        
+        return cell
+    }
+}
